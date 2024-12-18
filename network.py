6210e455d3930de4c328669ed538d7ea53a96a75
Arpit Rath <93031760+Hackmey@users.noreply.github.com>
2024-12-18 12:04:49 +0530

    Update network.py

--- network.py
+++ network.py
@@ -1,13 +1,14 @@
 import socket
 import pickle
 
+
 class Network:
     def __init__(self):
-        self.client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.server="192.168.184.165"
-        self.port = 5050
-        self.addr=(self.server,self.port)
-        self.p=self.connect()
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.1.4"
+        self.port = 5010
+        self.addr = (self.server, self.port)
+        self.p = self.connect()
 
     def getP(self):
         return self.p
@@ -15,14 +16,14 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048*8).decode()
+            return self.client.recv(2048).decode()
         except:
             pass
 
-    def send(self,data):
+    def send(self, data):
         try:
             self.client.send(str.encode(data))
-            return pickle.loads(self.client.recv(2048))
-        except EOFError:
-            print('An EOFError exception occurred. The file is empty')
+            return pickle.loads(self.client.recv(2048*2))
+        except socket.error as e:
+            print(e)
 
